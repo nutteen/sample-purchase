9f9b97108eead0c157cc7a1ca8ccdf437b5a4f9b
Peeranut <nutteen@gmail.com>
2022-11-09 00:03:46 +0700

    [purchase] Added validation in the request for creating purchase order

--- pkg/model/create_purchase_order.go
+++ pkg/model/create_purchase_order.go
@@ -3,13 +3,13 @@ package model
 import "github.com/shopspring/decimal"
 
 type CreatePurchaseOrderRequest struct {
-	PurchaseOrderItems []CreatePurchaseOrderItem `json:"purchaseOrderItems"`
+	PurchaseOrderItems []CreatePurchaseOrderItem `json:"purchaseOrderItems" validate:"required,min=1"`
 }
 
 type CreatePurchaseOrderItem struct {
-	ItemID	string `json:"itemId"`
-	ItemPrice	decimal.Decimal `json:"itemPrice"`
-	Quantity	int `json:"quantity"`
+	ItemID	string `json:"itemId" validate:"required"`
+	ItemPrice	decimal.Decimal `json:"itemPrice" validate:"required"`
+	Quantity	int `json:"quantity" validate:"required"`
 }
 
 type CreatePurchaseOrderResponse struct {
